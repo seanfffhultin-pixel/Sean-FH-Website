ec542e55981aa0aa9f405eae81bd9bde0446696d
Sean Hultin <seanhultin@Mac-3.broadband>
2025-11-26 21:56:16 +0000

    Add updated podcast section styling and navbar improvements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="nav-container">
@@ -10,11 +12,21 @@ export default function Navbar() {
           Sean FH Travels
         </Link>
 
+        {/* Hamburger Icon */}
+        <div
+          className={`hamburger ${menuOpen ? "active" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
         {/* Menu */}
-        <ul className="nav-menu">
-          <li><Link to="/" className="nav-link">Home</Link></li>
-          <li><Link to="/about" className="nav-link">About</Link></li>
-          <li><Link to="/reviews" className="nav-link">Reviews</Link></li>
+        <ul className={`nav-menu ${menuOpen ? "open" : ""}`}>
+          <li><Link to="/" className="nav-link" onClick={() => setMenuOpen(false)}>Home</Link></li>
+          <li><Link to="/about" className="nav-link" onClick={() => setMenuOpen(false)}>About</Link></li>
+          <li><Link to="/reviews" className="nav-link" onClick={() => setMenuOpen(false)}>Reviews</Link></li>
         </ul>
       </div>
     </nav>
