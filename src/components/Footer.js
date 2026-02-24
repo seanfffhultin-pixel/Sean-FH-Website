a308c13b743e90a06b2af34d5353f652c7f2bda1
Sean Hultin <seanhultin@Mac.broadband>
2026-02-24 19:18:58 +0000

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
         <a href="/privacy" className="footer-link">Privacy</a>
       </div>
 
-      <img src="/images/logo512 (2).png" alt="Sean FH Logo" className="footer-logo" />
+      <img src="/images/logo512 (1).png" alt="Sean FH Logo" className="footer-logo" />
     </footer>
   );
 }
